b59d4e77754d3ff5fffb9eee3aa4088f53f1f593
Sameh Elalfi <sameh.elalfi.mail@gmail.com>
2022-07-10 07:06:02 +0200

    installed react-icons

--- apps/fast-metro/app/src/screens/SplashScreen/SplashScreen.tsx
+++ apps/fast-metro/app/src/screens/SplashScreen/SplashScreen.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-
+import { FcHome } from 'react-icons/fc';
 export const SplashScreen = () => {
-  return <div>SplashScreen</div>;
+  return (
+    <div>
+      SplashScreen
+      <FcHome size={64} />
+    </div>
+  );
 };
