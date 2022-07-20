c03c21865b66a0b6a4593173b020b80e8b226ce4
Sameh Elalfi <sameh.elalfi.mail@gmail.com>
2022-07-21 00:21:41 +0200

    create basic structure for map screen

--- apps/fast-metro/app/src/screens/MapScreen/MapScreen.tsx
+++ apps/fast-metro/app/src/screens/MapScreen/MapScreen.tsx
@@ -1,5 +1,16 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
+import Page from '../../components/layout/Page/Page';
 
+import PageHeader from '../../components/shared/PageHeader/PageHeader';
 export const MapScreen = () => {
-  return <div>MapScreen</div>;
+  const { t } = useTranslation();
+
+  return (
+    <Page>
+      <div className="flex flex-col gap-10">
+        <PageHeader text={t`map-screen.title`} isBack={false} />
+      </div>
+    </Page>
+  );
 };
