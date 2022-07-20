8e4b873f8acfa4a74c5b6aa83bd21a1af6ba502c
Sameh Elalfi <sameh.elalfi.mail@gmail.com>
2022-07-21 00:22:11 +0200

    add className to page component

--- apps/fast-metro/app/src/components/layout/Page/Page.tsx
+++ apps/fast-metro/app/src/components/layout/Page/Page.tsx
@@ -7,9 +7,17 @@ import React from 'react';
  * @returns A React component that renders a div with the className "overflow-x-hidden bg-gray-50 p-8"
  * and the children passed in as props.
  */
-export const Page = ({ children }: { children: React.ReactNode }) => {
+export const Page = ({
+  children,
+  className = '',
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
   return (
-    <div className="max-w-screen h-screen overflow-x-hidden bg-gray-50 p-4 pb-28">
+    <div
+      className={`max-w-screen h-screen overflow-x-hidden bg-gray-50 p-4 pb-28 ${className}`}
+    >
       {children}
     </div>
   );
