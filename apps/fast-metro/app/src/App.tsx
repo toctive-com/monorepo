86e26914610cc778e4ea890fb7f84347bf9c14f1
Sameh Elalfi <sameh.elalfi.mail@gmail.com>
2022-07-20 19:11:53 +0200

    create transition between pages

--- apps/fast-metro/app/src/App.tsx
+++ apps/fast-metro/app/src/App.tsx
@@ -1,4 +1,6 @@
-import { Route, Routes } from 'react-router-dom';
+import React, { useRef, useEffect } from 'react';
+import { Route, Routes, useLocation } from 'react-router-dom';
+import { gsap } from 'gsap';
 
 import { GetStartedScreen } from './screens/GetStartedScreen';
 import { HomeScreen } from './screens/HomeScreen';
@@ -22,42 +24,62 @@ import { TransitStationsScreen } from './screens/TransitStationsScreen';
 import { ViolationsAndFinesScreen } from './screens/ViolationsAndFinesScreen';
 
 export function App() {
+  const pageRef = useRef(null);
+
+  // To get the current location (e.g. /home or /start-trip)
+  // we use this hook to execute the next useEffect to make transitions
+  // between pages
+  const location = useLocation();
+
+  /* A useEffect hook that is executed when the location changes. It is used to make transitions between
+pages. */
+  useEffect(() => {
+    let transition: gsap.core.Tween;
+    if (pageRef.current) {
+      transition = gsap.from(pageRef.current, {
+        x: 30,
+        opacity: 0,
+      });
+    }
+    return () => {
+      transition.kill();
+    };
+  }, [location.pathname]);
+
   return (
-    <Routes>
-      <Route path="/" element={<GetStartedScreen />} />
-      <Route path="/home" element={<HomeScreen />} />
-      <Route path="/map" element={<MapScreen />} />
-      <Route path="/settings" element={<SettingsScreen />} />
-
-      <Route path="/contact-metro" element={<ContactMetroScreen />} />
-      <Route path="/contact-us" element={<ContactUsScreen />} />
-      <Route path="/about-us" element={<AboutUsScreen />} />
-
-      <Route path="/start-trip">
-        <Route index element={<StartTripScreen />} />
-        <Route path="see-all-stations" element={<SeeAllStations />} />
-      </Route>
-
-      <Route path="/trip-stations" element={<TripStationsScreen />} />
-      <Route
-        path="/closest-transit-stations"
-        element={<ClosestTransitStationsScreen />}
-      />
-
-      <Route path="/metro-schedules" element={<MetroSchedulesScreen />} />
-      <Route
-        path="/safety-instructions"
-        element={<SafetyInstructionsScreen />}
-      />
-      <Route path="/station-services" element={<StationServicesScreen />} />
-      <Route path="/subscription" element={<SubscriptionScreen />} />
-      <Route path="/transit-stations" element={<TransitStationsScreen />} />
-      <Route
-        path="/violations-and-fines"
-        element={<ViolationsAndFinesScreen />}
-      />
-      <Route path="*" element={<p>404</p>} />
-    </Routes>
+    <main ref={pageRef}>
+      <Routes>
+        <Route path="/" element={<GetStartedScreen />} />
+        <Route path="/home" element={<HomeScreen />} />
+        <Route path="/map" element={<MapScreen />} />
+        <Route path="/settings" element={<SettingsScreen />} />
+        <Route path="/contact-metro" element={<ContactMetroScreen />} />
+        <Route path="/contact-us" element={<ContactUsScreen />} />
+        <Route path="/about-us" element={<AboutUsScreen />} />
+        <Route path="/start-trip">
+          <Route index element={<StartTripScreen />} />
+          <Route path="see-all-stations" element={<SeeAllStations />} />
+        </Route>
+        <Route path="/trip-stations" element={<TripStationsScreen />} />
+        <Route
+          path="/closest-transit-stations"
+          element={<ClosestTransitStationsScreen />}
+        />
+        <Route path="/metro-schedules" element={<MetroSchedulesScreen />} />
+        <Route
+          path="/safety-instructions"
+          element={<SafetyInstructionsScreen />}
+        />
+        <Route path="/station-services" element={<StationServicesScreen />} />
+        <Route path="/subscription" element={<SubscriptionScreen />} />
+        <Route path="/transit-stations" element={<TransitStationsScreen />} />
+        <Route
+          path="/violations-and-fines"
+          element={<ViolationsAndFinesScreen />}
+        />
+        <Route path="*" element={<p>404</p>} />
+      </Routes>
+    </main>
   );
 }
 
