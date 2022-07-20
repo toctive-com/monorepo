25860dbb98e88eef7d033131da87b10b090f3e06
Sameh Elalfi <sameh.elalfi.mail@gmail.com>
2022-07-21 00:21:11 +0200

    refactor and translate home screen

--- apps/fast-metro/app/src/screens/HomeScreen/HomeScreen.tsx
+++ apps/fast-metro/app/src/screens/HomeScreen/HomeScreen.tsx
@@ -1,32 +1,40 @@
 import Features from '../../components/layout/Features/Features';
 import Page from '../../components/layout/Page/Page';
+import { useTranslation } from 'react-i18next';
 
 import { Card } from '../../components/shared/Card/Card';
 import PageHeader from '../../components/shared/PageHeader/PageHeader';
 
 export const HomeScreen = () => {
+  const { t } = useTranslation();
+  const cards = [
+    { label: t`home-screen.start-trip`, href: '/start-trip' },
+    { label: t`home-screen.station-services`, href: '/station-services' },
+    { label: t`home-screen.transit-stations`, href: '/transit-stations' },
+    { label: t`home-screen.safety-instructions`, href: '/safety-instructions' },
+    { label: t`home-screen.metro-schedules`, href: '/metro-schedules' },
+    {
+      label: t`home-screen.violations-and-fines`,
+      href: '/violations-and-fines',
+    },
+    {
+      label: t`home-screen.closest-transit`,
+      href: '/closest-transit-stations',
+    },
+    { label: t`home-screen.subscription-information`, href: '/subscription' },
+  ];
+
   return (
     <Page>
       <div className="flex flex-col gap-10">
-        <PageHeader text="Home" isBack={false} />
+        <PageHeader text={t`home-screen.title`} isBack={false} />
 
         <Features>
-          {cards.map((item, i) => (
-            <Card text={item.label} to={item.href} />
+          {cards.map((item, index) => (
+            <Card text={item.label} to={item.href} key={index} />
           ))}
         </Features>
       </div>
     </Page>
   );
 };
-
-const cards = [
-  { label: 'Start Trip', href: '/start-trip' },
-  { label: 'Station services', href: '/station-services' },
-  { label: 'Transit Stations', href: '/transit-stations' },
-  { label: 'Safety Instructions', href: '/safety-instructions' },
-  { label: 'Metro Schedules', href: '/metro-schedules' },
-  { label: 'Violations and Fines', href: '/violations-and-fines' },
-  { label: 'Closest Transit', href: '/closest-transit-stations' },
-  { label: 'Subscription Information', href: '/subscription' },
-];
