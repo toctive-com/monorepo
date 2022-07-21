3dce7bcdc1046a41bfdf22f09aa69b97e4175bc0
Sameh Elalfi <sameh.elalfi.mail@gmail.com>
2022-07-21 02:38:02 +0200

    flip icon in rtl direction

--- apps/fast-metro/app/src/components/shared/PageHeader/PageHeader.tsx
+++ apps/fast-metro/app/src/components/shared/PageHeader/PageHeader.tsx
@@ -1,7 +1,8 @@
 import { useContext } from 'react';
 import { HiOutlineMenuAlt2 } from 'react-icons/hi';
-import { IoMdArrowBack } from 'react-icons/io';
+import { IoMdArrowBack, IoMdArrowForward } from 'react-icons/io';
 import { useNavigate } from 'react-router-dom';
+import { isRTL } from '../../../assets/js/appDirection';
 import { SideMenuContext } from '../../../hooks/SideMenuContext';
 
 interface PageHeaderI {
@@ -18,17 +19,32 @@ export function PageHeader({ className, text, isBack = true }: PageHeaderI) {
       className={`flex w-full items-center gap-5 rounded-xl px-8 py-3 shadow-sm ${className}`}
     >
       {isBack ? (
-        <IoMdArrowBack
-          className="cursor-pointer text-3xl"
-          onClick={() => {
-            /* on click, it will navigate back one page. */
-            if (isBack) {
-              navigate(-1);
-            }
-          }}
-        />
+        isRTL() ? (
+          <IoMdArrowForward
+            className="cursor-pointer text-3xl"
+            onClick={() => {
+              /* on click, it will navigate back one page. */
+              if (isBack) {
+                navigate(-1);
+              }
+            }}
+          />
+        ) : (
+          <IoMdArrowBack
+            className="cursor-pointer text-3xl"
+            onClick={() => {
+              /* on click, it will navigate back one page. */
+              if (isBack) {
+                navigate(-1);
+              }
+            }}
+          />
+        )
       ) : (
-        <HiOutlineMenuAlt2 className="text-3xl" onClick={toggleSideMenu} />
+        <HiOutlineMenuAlt2
+          className={`text-3xl rtl:scale-x-[-1]`}
+          onClick={toggleSideMenu}
+        />
       )}
       <span className="text-xl">{text}</span>
     </div>
