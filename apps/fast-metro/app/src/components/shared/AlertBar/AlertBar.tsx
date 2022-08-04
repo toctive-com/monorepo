5519267c4c88f021f3cc01c17d5f573ae8288900
Sameh A. Elalfi <31380068+SamehElalfi@users.noreply.github.com>
2022-08-04 17:02:18 +0200

    refactor: remove unnecessary classNames

--- apps/fast-metro/app/src/components/shared/AlertBar/AlertBar.tsx
+++ apps/fast-metro/app/src/components/shared/AlertBar/AlertBar.tsx
@@ -8,15 +8,9 @@ interface AlertBar {
 
 const AlertBar = (props: AlertBar) => {
     return (
-        <div className={`mmv rounded-md bg-red-500 px-4 py-3 shadow-xl shadow-red-300 `}>
+        <div className={`rounded-md bg-red-500 px-4 py-3 shadow-xl shadow-red-300 `}>
             <Ticker direction="toRight" speed={10} mode="smooth">
-                {() => (
-                    
-                        <h1>
-                            {props.content}
-                        </h1>
-                    
-                )}
+                {() => (<h1>{props.content}</h1>)}
             </Ticker>
         </div>
     )
