e1870f32f892b1be1224953b638a76a707f305d4
Sameh Elalfi <sameh.elalfi.mail@gmail.com>
2022-08-06 01:33:14 +0200

    fix: rename use-cases folder

--- libs/iam/src/lib/use-cases/user/getUser.ts
+++ libs/iam/src/lib/use-cases/user/getUser.ts
@@ -3,7 +3,7 @@ import { UserI } from '../../core/interfaces';
 import { UserRepository } from '../../repositories/UserRepository';
 import { CrmRepository } from '../../repositories/CrmRepository';
 
-export default (UserRepository: UserRepository, CrmServices: CrmRepository) => {
+export default (UserRepository: UserRepository) => {
   const Execute = async (id: string) => {
     return await UserRepository.getById(id);
   };
