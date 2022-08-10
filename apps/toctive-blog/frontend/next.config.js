abff4afce65b6ec28cbd3fbb8b07816c0b85aed6
Sameh Elalfi <sameh.elalfi.mail@gmail.com>
2022-08-10 02:41:28 +0200

    fix: styled-components ssr error w/ babel

--- apps/toctive-blog/frontend/next.config.js
+++ apps/toctive-blog/frontend/next.config.js
@@ -10,6 +10,10 @@ const nextConfig = {
     // See: https://github.com/gregberge/svgr
     svgr: false,
   },
+  compiler: {
+    // Enables the styled-components SWC transform
+    styledComponents: true
+  }
 };
 
 module.exports = withNx(nextConfig);
