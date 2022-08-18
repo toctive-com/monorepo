14dd8747f97428210ef3bd52a75e73f1513974b0
Sameh Elalfi <sameh.elalfi.mail@gmail.com>
2022-08-18 20:30:23 +0200

    install moment and nprogress

--- apps/toctive-blog/frontend/pages/_app.tsx
+++ apps/toctive-blog/frontend/pages/_app.tsx
@@ -1,6 +1,23 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import './styles.css';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
+
+// import moment from 'moment';
+// import 'moment/locale/ar'; // without this line it didn't work
+// moment.locale('ar');
+
+import Router from 'next/router';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css'; //styles of nprogress
+NProgress.configure({ showSpinner: false });
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
+
+gsap.registerPlugin(ScrollTrigger);
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
