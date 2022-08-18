f0b845a2fa7d3189d29128b6ce2cbeb2109221a7
Sameh Elalfi <sameh.elalfi.mail@gmail.com>
2022-08-19 00:48:10 +0200

    Create tests for Time component Tests added: - should render successfully - should change time format successfully
    Changes to be committed:
    	modified:   apps/toctive-blog/frontend/components/shared/time/time.spec.tsx

--- apps/toctive-blog/frontend/components/shared/time/time.spec.tsx
+++ apps/toctive-blog/frontend/components/shared/time/time.spec.tsx
@@ -1,10 +1,25 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import moment from 'moment';
 
 import Time from './time';
 
 describe('Time', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<Time time={new Date(Date.now())} />);
+    const time = new Date(Date.now());
+
+    const { baseElement } = render(<Time time={time} />);
     expect(baseElement).toBeTruthy();
+  });
+
+  it('should change time format successfully', () => {
+    const time = new Date(Date.now());
+    const defaultTimeFormat = moment(time, 'YYYYMMDD').fromNow();
+    const MomentTimeFormat = moment(time, 'YYYYMMDD').format('MMMM Do YYYY');
+
+    const { baseElement } = render(<Time time={time} />);
+    expect(baseElement.textContent).toEqual(MomentTimeFormat);
+
+    fireEvent.click(screen.getByText(MomentTimeFormat));
+    expect(baseElement.textContent).toEqual(defaultTimeFormat);
   });
 });
