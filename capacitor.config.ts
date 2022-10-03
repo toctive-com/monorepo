8e777d6bce32f2aea15ee2edd5b65548df7415d1
Sameh Elalfi <sameh.elalfi.mail@gmail.com>
2022-10-03 20:17:48 +0200

    fix: change the app id on google play

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.toctive.truemetro',
+  appId: 'com.toctive.metro',
   appName: 'True Metro',
   webDir: './dist/apps/true-metro/app',
   bundledWebRuntime: false,
