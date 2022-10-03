e429847057eec2f8c87c415f42e9f69e5663f57a
Sameh Elalfi <sameh.elalfi.mail@gmail.com>
2022-10-03 20:36:57 +0200

    add route to check the latest version of the app

--- apps/true-metro/backend/src/main.ts
+++ apps/true-metro/backend/src/main.ts
@@ -20,6 +20,14 @@ app.get('/api', (req, res) => {
 app.use('/api/v1/services', servicesRoutes);
 app.use('/api/v1/feedback', feedbackRoutes);
 
+// It is used by the app to check if the app needs to be updated
+// When the user opens the app, the app will check if the version
+// is the same as the one in the database and if not,
+// it will update the app
+app.use('/api/v1/latest-version', (req, res) => {
+  res.send({ version: '2.0.0' });
+});
+
 const port = process.env.port || 3333;
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
