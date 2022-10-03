4803c4ad7bbd9744a6910589d68bd5acdf6833b5
Mohammed-fahmy <mohammed.fahmy1@outlook.com>
2022-10-03 17:27:43 +0200

    change name to true metro

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.toctive.truemetro',
-  appName: 'Fast Metro',
+  appName: 'True Metro',
   webDir: './dist/apps/true-metro/app',
   bundledWebRuntime: false,
 };
