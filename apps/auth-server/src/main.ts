75203a60cde75ffe81a1e34502fe22b54ae7bfd2
Sameh Elalfi <sameh.elalfi.mail@gmail.com>
2022-11-11 22:11:11 +0200

    connect database and handle errors

--- apps/auth-server/src/main.ts
+++ apps/auth-server/src/main.ts
@@ -1,7 +1,21 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 import app from './app/app';
 
-const port = process.env.port || 3333;
-const server = app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/api`);
+// Load environment variables at apps/auth-server/.env
+dotenv.config();
+
+const mongodbURI = process.env.MONGODB_URL || '';
+mongoose.connect(mongodbURI, (err) => {
+  if (err) {
+    throw err;
+  }
+
+  console.log(`Connected to MongoDB at ${mongodbURI}`);
+
+  const PORT = process.env.PORT || 3000;
+  const server = app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+  server.on('error', console.error);
 });
-server.on('error', console.error);
