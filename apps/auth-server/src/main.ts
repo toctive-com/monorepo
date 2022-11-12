b7f25e81862a5f539a87197604ed384a4457c02b
Sameh Elalfi <sameh.elalfi.mail@gmail.com>
2022-11-12 19:32:46 +0200

    refactor: add comments and TODOs to main .ts

--- apps/auth-server/src/main.ts
+++ apps/auth-server/src/main.ts
@@ -5,17 +5,21 @@ import app from './app/app';
 // Load environment variables at apps/auth-server/.env
 dotenv.config();
 
+// Connect to MongoDB and start the server
 const mongodbURI = process.env.MONGODB_URL || '';
 mongoose.connect(mongodbURI, (err) => {
   if (err) {
     throw err;
   }
-
   console.log(`Connected to MongoDB at ${mongodbURI}`);
 
+  // Start the server
   const PORT = process.env.PORT || 3000;
   const server = app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port http://localhost:${PORT}`);
   });
+
+  // if there is an error on the server we can see it
+  // TODO: log error using winston logger or logger server
   server.on('error', console.error);
 });
