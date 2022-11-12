24e0c8990b1b6c828029aae44f7a25cb2c17d93a
Sameh Elalfi <sameh.elalfi.mail@gmail.com>
2022-11-12 19:51:15 +0200

    refactor: add docs to emailValidator middleware

--- apps/auth-server/src/middlewares/auth/validators/email.ts
+++ apps/auth-server/src/middlewares/auth/validators/email.ts
@@ -1,6 +1,28 @@
 import { NextFunction, Request, Response } from 'express';
 import isemail from 'isemail';
-
+/**
+ * Email validator middleware
+ * This middleware will validate the email field in the request body.
+ *
+ * @example ```js
+ * // valid usernames
+ * 'john@example.com'
+ * 'john.doe@example.com'
+ * 'john+something@example.com'
+ * 'john@localhost'
+ *
+ * // invalid usernames
+ * 'john doe'
+ * 'john.doe'
+ * 'john.example.com'
+ * 'john#doe'
+ * ```
+ *
+ * @param req
+ * @param res
+ * @param next
+ * @returns
+ */
 export const validateEmail = (
   req: Request,
   res: Response,
